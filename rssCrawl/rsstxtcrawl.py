52b4bb50a460ff6519881c2ccd7950896d4c5b27
YUICHI MACHIDA <ymachida@ymMBP.local>
2018-07-01 21:57:17 +0900

    とりあえず動作版完成

--- rssCrawl/rsstxtcrawl.py
+++ rssCrawl/rsstxtcrawl.py
@@ -1,16 +1,17 @@
 #経産省RSSのうち、新着情報のテキストをとってくる処理
-#BeautifulSoupの解析の仕方が悪いので、MetiJournalのURLだと動かないけど、MetiJournalっているんだっけ。
+'''
+フォルダ構成は以下の通り、BaseDirectoryの配下にhistoryとTextDataを作ってください。
+base directory  -
+                - histry d         - histry.txt
+                - TextData d       - METI_新着_2018xxxxxxx.txt
 
-#TODO
-#実行時間で新規ファイル作ってとってくるだけなので、前回取得からの差分取得に修正する必要あり。
-
-
+'''
 import os, datetime, sys
 import feedparser #RSS(xml)解析
 import urllib #URL接続
 from bs4 import BeautifulSoup #HTML解析
 
-#実行時刻を取得する
+#実行時刻を取得する（文字列で返す）
 def getDT():
         now = datetime.datetime.now()
         dt = "{0:%Y%m%d-%H%M%S}".format(now)
@@ -23,50 +24,91 @@ def getPageTxt(targetUrl):
         # 解析用のBeautifulSoupオブジェクト
         soup = BeautifulSoup(htmlData,'html.parser')
         # 以下テキスト化処理
-        title = soup.find(id='MainContentsArea') #タイトル, このidで全部うまくいくのか不明
+        titleText = soup.find(id='MainContentsArea') #タイトル, このidで全部うまくいくのか不明
         bodyText = soup.findAll('p') #本文、Soupのリストオブジェクト
-        plainText = title.string + '\n' #戻り値用のテキストにタイトルを書き込む
+        urlAllText = titleText.string + '\n' #戻り値用のテキストにタイトルを書き込む
         for i in bodyText:
                 if i.string is not None:
-                        plainText += str(i.string) + '\n' #Bodyのテキストを順次書き込み
+                        urlAllText += str(i.string) + '\n' #Bodyのテキストを順次書き込み
         print(targetUrl + " Text extracted.")
-        return plainText
+        return urlAllText
 
-#URLのリストをもとにTextに書き込みに行く処理（Soupでのテキスト抽出はgetPageTxt()）
+#URLのリストをもとにTextに書き込みに行く処理（テキスト抽出はgetPageTxt()）
 def writPageTxt(Links):
-        base = os.path.dirname(os.path.abspath(__file__))
-        path = base + '/' + 'METI_NR_' + getDT() + '.txt'
-        with open(path, mode='w', encoding='utf-8') as file:
-                for eachUrl in Links:
-                        file.write(getPageTxt(eachUrl) + '\n')
-        print("Saved at " + path)
+    textPath = savePath + 'METI_新着_' + getDT() + '.txt'
+    with open(textPath, mode='w', encoding='utf-8') as file:
+        for eachUrl in Links:
+            file.write(getPageTxt(eachUrl[1]) + '\n')
+    print("Saved at " + textPath)
 
-#RSSの中のLinkをリストで取得する
+#RSSの中のLinkとUpdateをリストinリストで取得する
 def getRssLinks():
-    rssUrls = [
-        "http://www.meti.go.jp/ml_index_release_atom.xml",
-        #「RSSのURLはここに追加」にしようとおもったけど、新着情報しかテキスト化できてないからあまり意味がない
-        ]
+    rssUrl = "http://www.meti.go.jp/ml_index_release_atom.xml"
     rssLinks = [] #URL格納用空リスト
-    upDated = [] #??差分取得用のリストでも作ったらどうだろうか。
-    for rssUrl in rssUrls:
-        rssDic = feedparser.parse(rssUrl) #feedparserでひとつのRSSを辞書化
-        for entry in rssDic.entries:
-            rssLinks.append(entry.link) #RSSの中のLinkをリストに追加
+    rssDic = feedparser.parse(rssUrl) #feedparserでひとつのRSSを辞書化
+    for entry in rssDic.entries:
+        rssLinks.append([entry.updated, entry.link]) #RSSの中のLinkをリストに追加
 
     print("URL lists are corrected.\nURLs are")
     for i in rssLinks:
-            print(i)
+        print(i[0]+', '+i[1])
     return rssLinks #リストを返す
 
-#
+#Historyテキストがなければ作成
+def createHistory():
+    pathExist = False
+    if os.path.exists(historyPath):
+        if not os.path.exists(historyPath + 'history.txt'):
+            with open(historyPath + 'history.txt', mode='w', encoding='utf-8') as file:
+                file.write('This is xml update history list.' + '\n')
+            print('History text is created.')
+            pathExist = True
+        else:
+                print('History text is found.')
+                pathExist = True
+    else:
+        print('''
+        Create directories as below
+        base directory  - rssTxtCrawl.py
+		        - histry d         - histry.txt
+                        - TextData d       - METI_
+                        ''')
+    return pathExist
 
 #Histroyテキストの中のUpodateとURLのリストを突合する
-#def xmlHistoryCheck():
+def checkHistory(urls):
+    newUrls = []
+    with open(historyPath + 'history.txt', mode='r') as f:
+        lines = f.readlines()
+        for url in urls:
+                print(url[1])
+                checkDateUrl = url[0] + ', ' + url[1] + '\n'
+                if checkDateUrl in lines:
+                        print('True, ' + url[1] + ' is exist.')
+                else:
+                        print('Else, ' + url[1] + ' is appended.')
+                        newUrls.append(url)
+    with open(historyPath + 'history.txt', mode='a') as add:
+        for newUrl in newUrls:
+                add.writelines(newUrl[0]+ ', ' + newUrl[1] +'\n')
+    return newUrls
 
+#プログラム動作パス
+#任意のパスの場合
+#basePath = 'ここにパスを書く'
+basePath = os.path.dirname(os.path.abspath(__file__)) #デフォルトはこの.pyの実行フォルダ
+historyPath = basePath + r'/history/'
+savePath = basePath + r'/TextData/'
 
 #メイン処理
-urlList = getRssLinks()
-print("Text extracting...")
-writPageTxt(urlList)
-print("Done.")
+if createHistory():
+    urlList = getRssLinks()
+    print("Historyと突合中...")
+    updatedURL = checkHistory(urlList)
+    if len(updatedURL) == 0:
+            print('新着情報はありません')
+    else:
+            writPageTxt(updatedURL)
+    print('Done.')
+else:
+    print('履歴ファイルを保持するフォルダを作成してください。')
---
+++ rsstxtcrawl.py
@@ -0,0 +1,72 @@
+#経産省RSSのうち、新着情報のテキストをとってくる処理
+#BeautifulSoupの解析の仕方が悪いので、MetiJournalのURLだと動かないけど、MetiJournalっているんだっけ。
+
+#TODO
+#実行時間で新規ファイル作ってとってくるだけなので、前回取得からの差分取得に修正する必要あり。
+
+
+import os, datetime, sys
+import feedparser #RSS(xml)解析
+import urllib #URL接続
+from bs4 import BeautifulSoup #HTML解析
+
+#実行時刻を取得する
+def getDT():
+        now = datetime.datetime.now()
+        dt = "{0:%Y%m%d-%H%M%S}".format(now)
+        return dt
+
+#各ページのURLをもとにテキストを返す処理
+def getPageTxt(targetUrl):
+        # 記事のHTMLを取得
+        htmlData = urllib.request.urlopen(targetUrl).read().decode('utf-8')
+        # 解析用のBeautifulSoupオブジェクト
+        soup = BeautifulSoup(htmlData,'html.parser')
+        # 以下テキスト化処理
+        title = soup.find(id='MainContentsArea') #タイトル, このidで全部うまくいくのか不明
+        bodyText = soup.findAll('p') #本文、Soupのリストオブジェクト
+        plainText = title.string + '\n' #戻り値用のテキストにタイトルを書き込む
+        for i in bodyText:
+                if i.string is not None:
+                        plainText += str(i.string) + '\n' #Bodyのテキストを順次書き込み
+        print(targetUrl + " Text extracted.")
+        return plainText
+
+#URLのリストをもとにTextに書き込みに行く処理（Soupでのテキスト抽出はgetPageTxt()）
+def writPageTxt(Links):
+        base = os.path.dirname(os.path.abspath(__file__))
+        path = base + r'/' + r'METI_NR_' + getDT() + r'.txt'
+        with open(path, mode='w', encoding='utf-8') as file:
+                for eachUrl in Links:
+                        file.write(getPageTxt(eachUrl) + '\n')
+        print("Saved at " + path)
+
+#RSSの中のLinkをリストで取得する
+def getRssLinks():
+    rssUrls = [
+        "http://www.meti.go.jp/ml_index_release_atom.xml",
+        #「RSSのURLはここに追加」にしようとおもったけど、新着情報しかテキスト化できてないからあまり意味がない
+        ]
+    rssLinks = [] #URL格納用空リスト
+    upDated = [] #??差分取得用のリストでも作ったらどうだろうか。
+    for rssUrl in rssUrls:
+        rssDic = feedparser.parse(rssUrl) #feedparserでひとつのRSSを辞書化
+        for entry in rssDic.entries:
+            rssLinks.append(entry.link) #RSSの中のLinkをリストに追加
+
+    print("URL lists are corrected.\nURLs are")
+    for i in rssLinks:
+            print(i)
+    return rssLinks #リストを返す
+
+#
+
+#Histroyテキストの中のUpodateとURLのリストを突合する
+#def xmlHistoryCheck():
+
+
+#メイン処理
+urlList = getRssLinks()
+print("Text extracting...")
+writPageTxt(urlList)
+print("Done.")
